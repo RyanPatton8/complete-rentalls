fa7e5ab05fdc657afcbf626745c5d32f7e9b5591
Ryan Patton <ryanpatton173@gmail.com>
2025-11-29 09:36:27 -0500

    added link based navigation

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -4,14 +4,15 @@ const mainFooter = document.querySelector("#main-footer");
 // insert all of the elements i want inside them
 mainHeader.innerHTML = `
         <div class="container header-container">
-        <a href="index.html"><img src="./imgs/main-logo.png" alt="Young Drivers logo" class="header-logo"/>
+        <a href="index.html#"><img src="./imgs/main-logo.png" alt="Young Drivers logo" class="header-logo"/>
         </a>
         <nav>
                 <ul class="nav-links">
-                    <li><a href="index.html">Home</a></li>
+                    <li><a href="index.html#">Home</a></li>
                     <li><a href="product-categories.html?category=Equipment">Equipment</a></li>
                     <li><a href="product-categories.html?category=Events">Events</a></li>
-                    <li><a href="index.html#contact-us" class="btn">Contact Us</a></li>
+                    <li><a href="index.html#contact-us-holder" class="btn">Contact Us</a></li>
+                    <li><a href="index.html#about-us-holder" class="btn">About Us</a></li>
                 </ul>
             </nav>
             <div class="mobile-menu">&#9776;</div>
