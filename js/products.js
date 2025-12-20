abf847469f3d8f80c661b1148b2c4413cef34a39
completerentallsorillia1 <completerentallsorillia1@gmail.com>
2025-12-20 15:04:29 -0500

    added some augers and cleaners

--- js/products.js
+++ js/products.js
@@ -3,19 +3,120 @@
 //       "id": "14",
 //       "name": "ITEM",
 //       "prices": [
+//         { "label": "4 Hour",  "amount": "DOLLARS"},
 //         { "label": "1 Day",  "amount": "DOLLARS"},
 //         { "label": "Weekend",  "amount": "DOLLARS" },
 //         { "label": "1 Week", "amount": "1,880.00" },
 //         { "label": "4 Weeks", "amount": "5,170.00" }
 //       ],
-//       "img": "filepath",
+//       "img": "./imgs/products/equipment or events/category/imagename.jpg",
 //       "model": "ITEM MODEL",
 //       "desc": "ADDITIONAL DESCRIPTION"
+//     },
+//   ],
+
+// "Category": [
+//     {
+//       "id": "14",
+//       "name": "ITEM",
+//       "prices": [
+//         { "label": "4 Hour",  "amount": "DOLLARS"},
+//         { "label": "1 Day",  "amount": "DOLLARS"},
+//         { "label": "Weekend",  "amount": "DOLLARS" },
+//         { "label": "1 Week", "amount": "1,880.00" },
+//         { "label": "4 Weeks", "amount": "5,170.00" }
+//       ],
+//       "img": "./imgs/products/equipment or events/category/imagename.jpg",
+//       "model": "ITEM MODEL",
+//       "desc": `<ul>
+        //   <li>list item 1</li>
+        //   <li>list item 2</li>
+        //   <li>list item 3</li>
+        // </ul>`
 //     },
 //   ],
   
 const products = {
   "Equipment": {
+"Augers": [
+    {
+      "id": "76",
+      "name": "Clam Shovel",
+      "prices": [
+        { "label": "1 Day",  "amount": "15.00"},
+        { "label": "Weekend",  "amount": "22.50" },
+        { "label": "1 Week", "amount": "60.00" },
+        { "label": "4 Weeks", "amount": "180" }
+      ],
+      "img": "./imgs/products/equipment/Augers/clam.jpg",
+    },
+    {
+      "id": "77",
+      "name": "Power Post Hole Auger – 1 Man",
+      "prices": [
+        { "label": "1 Day",  "amount": "110.00"},
+        { "label": "Weekend",  "amount": "165.00" },
+        { "label": "1 Week", "amount": "440.00" },
+        { "label": "4 Weeks", "amount": "1320.00" }
+      ],
+      "img": "./imgs/products/equipment/Augers/bhydauger.gif",
+      "desc": `<ul>
+          <li>Towable (2 inch ball required)</li>
+          <li>8″, 10″ or 12″ bit avaiable</li>
+          <li>Extension – $5.00 (if hole is 4' deep)</li>
+        </ul>`
+    },
+  ],
+    "Air Compressors & Accessories" : [
+      {
+        "id": "13",
+        "name": "Air Compressor 185 CFM (Diesel)",
+        "prices": [
+          { "label": "1 Day",  "amount": 140.00 },
+          { "label": "Weekend",  "amount": 210.00 },
+          { "label": "1 Week", "amount": 580.00 },
+          { "label": "4 Weeks", "amount": "1,400.00" }
+        ],
+        "img": "./imgs/products/equipment/Air Compressors & Accessories/IR185compressor.gif",
+      },
+      {
+        "id": "3",
+        "name": "Air Compressor 6 CFM (Electric)",
+        "prices": [
+          { "label": "4 Hours",  "amount": 25.00 },
+          { "label": "1 Day",  "amount": 35.00 },
+          { "label": "Weekend", "amount": 52.50 },
+          { "label": "1 Week", "amount": 140.00 },
+          { "label": "4 Weeks", "amount": 350.00 },
+        ],
+        "img": "./imgs/products/equipment/Air Compressors & Accessories/compressorp.jpg"
+      },
+    ], 
+    "Cleaning": [
+    {
+      "id": "67",
+      "name": "Pressure Washer 2000 PSI (Gas)",
+      "prices": [
+        { "label": "4 hour", "amount": "55.00" },
+        { "label": "1 Day",  "amount": "75.00"},
+        { "label": "Weekend",  "amount": "112.50" },
+        { "label": "1 Week", "amount": "300.00" },
+        { "label": "4 Weeks", "amount": "750.00" }
+      ],
+      "img": "./imgs/products/equipment/cleaning/bwasher.jpg",
+    },
+        {
+      "id": "14",
+      "name": "Magnet Sweeper",
+      "prices": [
+        { "label": "1 Day",  "amount": "25.00"},
+        { "label": "Weekend",  "amount": "37.50" },
+        { "label": "1 Week", "amount": "100.00" },
+        { "label": "4 Weeks", "amount": "300.00" }
+      ],
+      "img": "./imgs/products/equipment/cleaning/bmagsweeper-600x600.jpg",
+    },
+  ],
     "Excavators": [
       {
         "id": "13",
@@ -43,34 +144,35 @@ const products = {
         "desc": "Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text,"
       },
     ],
-    "Air Compressors & Accessories" : [
-      {
-        "id": "13",
-        "name": "Air Compressor 185 CFM (Diesel)",
-        "prices": [
-          { "label": "1 Day",  "amount": 140.00 },
-          { "label": "Weekend",  "amount": 210.00 },
-          { "label": "1 Week", "amount": 580.00 },
-          { "label": "4 Weeks", "amount": "1,400.00" }
-        ],
-        "img": "./imgs/products/equipment/Air Compressors & Accessories/IR185compressor.gif",
-      },
-      {
-        "id": "3",
-        "name": "Air Compressor 6 CFM (Electric)",
-        "prices": [
-          { "label": "4 Hours",  "amount": 25.00 },
-          { "label": "1 Day",  "amount": 35.00 },
-          { "label": "Weekend", "amount": 52.50 },
-          { "label": "1 Week", "amount": 140.00 },
-          { "label": "4 Weeks", "amount": 350.00 },
-        ],
-        "img": "./imgs/products/equipment/Air Compressors & Accessories/compressorp.jpg"
-      },
-    ], 
+    
   },
 
   "Events": {
-
+    "TENTS AND CANOPIES": [
+    {
+      "id": "15",
+      "name": "40’x100′ Pole Tent",
+      "prices": [
+        { "label": "Weekend",  "amount": "3,100.00"},
+      ],
+      "img": "./imgs/products/events/TENTS AND CANOPIES/40X100.jpg",
+    },
+    {
+      "id": "14",
+      "name": "40’x120′ Pole Tent",
+      "prices": [
+        { "label": "Weekend",  "amount": "3,700.00"},
+      ],
+      "img": "./imgs/products/events/TENTS AND CANOPIES/4080.jpg",
+    },
+    {
+      "id": "16",
+      "name": "40’x80′ Pole Tent",
+      "prices": [
+        { "label": "Weekend",  "amount": "2,600.00"},
+      ],
+      "img": "./imgs/products/events/TENTS AND CANOPIES/40-x-80-water-pic.jpg",
+    },
+  ],
   }
 };
